5ebdc902e787c2583924ff02d45c7009c3888cce
Armaro <37552371+armaro@users.noreply.github.com>
2018-03-22 10:53:31 +0430

    update selecting dummy feature
    dummy feature is now edited to be taken correctly.

--- pandasDummies.py
+++ pandasDummies.py
@@ -1,6 +1,5 @@
 def fix_concat_dummies(dummyName, data):
-  var = pd.get_dummies([dummyName], drop_first=True)
+  var = pd.get_dummies(data[dummyName], drop_first=True)
   data = pd.concat([data,var], axis=1)
   data = data.drop(dummyName, axis=1)
   return data
-  
